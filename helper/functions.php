b882d1a0c1c77c8a28889d672ae61b9ba2810655
Hector Coello <hector_m3@live.com.mx>
2022-10-27 11:45:08 -0500

    Función para obtener post o get

--- helper/functions.php
+++ helper/functions.php
@@ -5,4 +5,14 @@ function param($pos){
     return isset($params[$pos]) ? $params[$pos] : "";
 }
 
+function input($name, $type="all"){
+    $value= "";
+    if($type=="get" || $type=="all"){
+        $value = isset($_GET['tk']) ? $_GET['tk'] : "";
+    }elseif ( ($type=="post" || $type=="all") && $value=="" ) {
+        $value = isset($_POST['tk']) ? $_POST['tk'] : "";
+    }
+    return $value;
+}
+
 
